2be6f896d20705337de7ce2f87efabc12f720684
Danilo <danilo.guimaraes.carvalho@gmail.com>
2020-12-02 23:42:50 -0300

    correções Layout

--- src/inicial/index.js
+++ src/inicial/index.js
@@ -1,8 +1,6 @@
 import 'date-fns';
 import React, { useState, useEffect } from "react";
-import { Container, FormControl, InputLabel, Select, MenuItem, Grid, TextField, Button,} from "@material-ui/core";
-import DateFnsUtils from '@date-io/date-fns';
-import {  MuiPickersUtilsProvider, KeyboardDatePicker,} from "@material-ui/pickers";
+import { Container, FormControl, InputLabel, Select, MenuItem, Grid, TextField, Button, } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import SearchIcon from '@material-ui/icons/Search';
 import axios from 'axios';
@@ -11,37 +9,71 @@ import axios from 'axios';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    background: 'linear-gradient(45deg, #40E0D0 30%, #7FFFD4 90%)',    
+    background: 'linear-gradient(45deg, #40E0D0 30%, #7FFFD4 90%)',
     button: {
-      margin: theme.spacing(1),      
+      margin: theme.spacing(1),
     },
-      width: "100%", 
-      color: "#4f40b3",
-      
+    width: "100%",   
+    color: "#4f40b3",    
+    margin:'auto',
+    webkitBoxShadow: " 3px 6px 20px 1px rgba(0,0,0,0.75)",
+    mozBoxShadow: " 3px 6px 20px 1px rgba(0,0,0,0.75)",
+    boxShadow: "3px 6px 20px 1px rgba(0,0,0,0.75)",
+    fontFamily:"roboto",    
+  },
+
+  button:{
+    marginLeft:"6%",
+    width:"100%",
+  },
+
+  datas:{
+    
+    justifyContent:'center',
+    marginRight:"5%",
+    marginBottom:"2%",
+    width:"100%",
+    
   },
 
   formControl: {
-    minWidth: 170,    
+    minWidth: "192px",
   },
-  formControlPassageiro: {
-    minWidth: 170,
-    
-  }
+
+  IdaVolta:{
+    marginLeft:"9%",
+    marginBottom:"2%",
+  },
+
+  textField:{
+   width:"100%",
+  },
+
+  passageiros:{
+    marginLeft:"8%",
+    width:"100%",
+   },
+
+  tittle:{
+    textAlign:'center',
+  },
+  
 }));
 
 export const Inicio = (props) => {
   const classes = useStyles();
   const [origem, setOrigem] = useState("");
-  const [destino, setDestino] = useState("");
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [destino, setDestino] = useState("");  
   const [classe, setClasse] = useState("");
   const [locais, setLocais] = useState([]);
 
-  useEffect(()=> {
+ 
+  useEffect(() => {
     //recebe a URL da api como parametro
-      axios.get('https://localhost:44399/api/Local')
-      .then(resultado => setLocais(resultado.data))
-  }, [] );
+    axios.get('https://localhost:44399/api/Local')
+      .then(resultado => setLocais(resultado.data))// status 200 ok
+      .catch(erro => console.log(erro) ) //status 500 ou 400.
+  }, []);
 
   const changeOrigem = (e) => {
     setOrigem(e.target.value);
@@ -49,10 +81,6 @@ export const Inicio = (props) => {
 
   const changeDestino = (e) => {
     setDestino(e.target.value);
-  };
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
   };
 
   const changeClasse = (e) => {
@@ -65,7 +93,7 @@ export const Inicio = (props) => {
     { id: 3, nome: "Aeroporto Aeroclube do Espirito Santo - ES" },
     { id: 4, nome: "Aeroporto de Brasilia" },
   ];*/
-  
+
   const vooClasses = [
     { id: 1, nome: "Econômica" },
     { id: 2, nome: "Primera" },
@@ -74,9 +102,9 @@ export const Inicio = (props) => {
 
   return (
     <div className={classes.root}>
-    <Container maxWidth="sm">
-    <h1>{"Aeroporto API"}</h1>
-      <Grid container spacing={3}>
+      <Container maxWidth="sm">
+        <h1 className={classes.tittle} > {"Aeroporto API"}</h1>
+        <Grid  className={classes.IdaVolta} container spacing={3}>
           <Grid item xs={12}>
             <FormControl className={classes.formControl}>
               <InputLabel id="origem">Origem</InputLabel >
@@ -88,13 +116,13 @@ export const Inicio = (props) => {
               >
                 {locais.map((item) => (
                   <MenuItem value={item.id}>{item.nome}</MenuItem>
-                ))}                
+                ))}
               </Select>
             </FormControl>
           </Grid>
         </Grid>
 
-        <Grid container spacing={3}>
+        <Grid className={classes.IdaVolta} container spacing={3}>
           <Grid item xs={12}>
             <FormControl className={classes.formControl}>
               <InputLabel id="destino">Destino</InputLabel>
@@ -112,49 +140,43 @@ export const Inicio = (props) => {
           </Grid>
         </Grid>
 
-  
-       <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-               disableToolbar
-               variant="inline"
-               format="dd/MM/yyyy"
-               margin="normal"
-               id="Data Ida"
-               label="Data Ida"
-               value={selectedDate}
-               onChange={handleDateChange}
-               KeyboardButtonProps={{
-                   'aria-label': 'change date',
-              }}
-            />
-            </MuiPickersUtilsProvider>
-          </Grid>
-        
 
-        
-          <Grid item xs={4}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-               disableToolbar
-               variant="inline"
-               format="dd/MM/yyyy"
-               margin="normal"
-               id="Data Volta"
-               label="Data Volta"
-               value={selectedDate}
-               onChange={handleDateChange}
-               KeyboardButtonProps={{
-                   'aria-label': 'change date',
-              }}
-            />
-            </MuiPickersUtilsProvider>
+        <Grid className={classes.datas} container spacing={2}>
+          <Grid item xs={4.5}>
+            <form className={classes.datas} noValidate>
+              <TextField
+                id="date"
+                label="Data Ida"
+                type="date"
+                defaultValue="" 
+                className={classes.textField}              
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </form>
+          </Grid>
+
+
+
+          <Grid item xs={4.5}>
+          <form className={classes.datas} noValidate>
+              <TextField
+                id="date"
+                label="Data Volta"
+                type="date"
+                defaultValue="" 
+                className={classes.textField}              
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </form>
           </Grid>
         </Grid>
 
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
+        <Grid  className={classes.passageiros} container spacing={3}>
+          <Grid item xs={5}>
             <TextField
               id="qtdPassageiros"
               label="Qtd. Passageiros"
@@ -165,7 +187,7 @@ export const Inicio = (props) => {
             />
           </Grid>
           <Grid item xs={4}>
-            <FormControl className={classes.formControlPassageiro}>
+            <FormControl className={classes.formControl}>
               <InputLabel id="classe">Classe</InputLabel>
               <Select
                 label="Classe"
@@ -181,14 +203,14 @@ export const Inicio = (props) => {
           </Grid>
         </Grid>
 
-        <Grid container spacing={5}>
+        <Grid className={classes.button} container spacing={5}>
           <Grid item xs={3}>
-            <Button variant="contained" color="primary"   className={classes.button}        endIcon={<SearchIcon />}>
+            <Button variant="contained" color="primary" className={classes.button} endIcon={<SearchIcon />}>
               Buscar
             </Button>
           </Grid>
         </Grid>
-    </Container>
+      </Container>
     </div >
   );
 };
